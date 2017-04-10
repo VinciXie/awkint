c1c1e3ef880d0fc6d63531321f887ba9fc58eb6c
VinciXie <szqq602@163.com>
2017-04-10 18:47:19 +0800

    保存一下

--- static/js/index.js
+++ static/js/index.js
@@ -1,32 +1,33 @@
 $(function(){
-    // $(".box").eq(0).click(function () {
-    //     // $(".tabContent").hide()
-    //     // $(".tabContent").eq(0).fadeToggle("slow","linear");
-    //
-    // })
+  $(".box").click(function(){
+    var $this = $(this),
+        data = $this[0];
+        index = data.dataset.boxnumber
+    // 点击的时候判断
+    var clearActive = function() {
+      $('.active').removeClass("active")
+      $('.rotate135').removeClass("rotate135")
+      $(".tabContent").slideUp()
+    }
+    if($this.hasClass("active")) {
+      clearActive()
+    } else {
+      clearActive()
+      // 当前是取消选中状态
+      // 激活操作
+      $this.addClass("active")
+      // 给子元素加上 rotate135
+      $this.children('span').addClass('rotate135')
+      // 对应的下拉框 展现
+      $(".tabContent").eq(index).slideDown()
+    }
+  });
 
-    $(".box").click(function(){
-        var $this = $(this),
-            data = $this[0];
-            index = data.dataset.boxnumber
-        // 点击的时候判断
-        var clearActive = function() {
-          $('.active').removeClass("active")
-          $('.rotate135').removeClass("rotate135")
-          $(".tabContent").slideUp()
-        }
-        if($this.hasClass("active")) {
-          clearActive()
-        } else {
-          clearActive()
-          // 当前是取消选中状态
-          // 激活操作
-          $this.addClass("active")
-          // 给子元素加上 rotate135
-          $this.children('span').addClass('rotate135')
-          // 对应的下拉框 展现
-          $(".tabContent").eq(index).slideDown()
-        }
-    });
+  $(".banner").unslider({
+    // autoplay: true,
+    arrows: false,
+    delay: 3600,
+    index: 1
+  })
 
 });
