1c0a3657f9921d510e995ab73bcb029ec1116166
VinciXie <szqq602@163.com>
2017-04-11 17:17:57 +0800

    整体样式基本改完

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,26 @@
 $(function(){
   $(".box").click(function(){
+    // 下拉菜单的效果
     var $this = $(this),
+        $parent = $this.parent()
         data = $this[0];
         index = data.dataset.boxnumber
     // 点击的时候判断
     var clearActive = function() {
-      $('.active').removeClass("active")
-      $('.rotate135').removeClass("rotate135")
-      $(".tabContent").slideUp()
+      console.log('clearActive');
+
+      if ($parent.find(".tabContent")) {
+        // 团队成员部分的下拉菜单
+        $parent.children('.active').removeClass("active")
+        $parent.find(".tabContent").slideUp()
+
+      }
+      if ($parent.next(".tabC")) {
+        // 主要产品部分的下拉菜单
+        var $tabC = $parent.next(".tabC")
+
+        $tabC.find(".tabContent").slideUp()
+      }
     }
     if($this.hasClass("active")) {
       clearActive()
@@ -16,23 +29,41 @@ $(function(){
       // 当前是取消选中状态
       // 激活操作
       $this.addClass("active")
-      // 给子元素加上 rotate135
-      $this.children('span').addClass('rotate135')
       // 对应的下拉框 展现
-      $(".tabContent").eq(index).slideDown()
+      if ($parent.find(".tabContent")) {
+        $parent.children(".tabContent").eq(index).slideDown()
+      }
+      if ($parent.next(".tabC")) {
+        var $tabC = $parent.next(".tabC")
+
+        $tabC.find(".tabContent").eq(index).slideDown()
+      }
     }
   });
 
+  // 有两个下拉菜单是先展示出来的
+  $(".members").find(".tabContent").eq(0).slideDown()
+  $(".tabC").find(".tabContent").eq(0).slideDown()
+
+// 轮播图
   $(".banner").unslider({
-    // autoplay: true,
+    autoplay: true,
     arrows: false,
-    delay: 3600,
-    index: 1
+    delay: 3000,
+    // index: 1,
   })
 
   $('.scrollup').click(function(){
+    // 点击回到顶部的动画
     $("html, body").animate({ scrollTop: 0 }, 1000);
     return false;
   });
 
+  // background 3 和 5 的高度 和 body 一样高
+  // $('body').height()
+  $('.background3,.background5').height(document.body.offsetHeight);
+
+  //
+  // $('#menu-main, .brand').localScroll();
+  $('.navbar-nav').localScroll();
 });
